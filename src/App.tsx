5c9073db2c03a8d0b4c8f4932199196a161dc83a
Vikram Chaudhary <chaudharyvikram411@gmail.com>
2025-10-23 17:09:44 +0530

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SwiftUIArchitectureExplorer from "./SwiftUIArchitectureExplorer";
 import Blogs from './Blogs';
 import BlogDetail from './BlogDetail';
